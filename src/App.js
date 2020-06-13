7ba63d35b639565855c64623e386b6fef438bcd4
dramein <damienfialhoj@gmail.com>
2020-06-13 12:25:14 +0100

    updated app.js

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Damien's Portfolio.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Dramein"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
       </header>
     </div>
